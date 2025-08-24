b742da808d0b2109fa5cedfc4827aeec04396c73
CrypticSafe5 <WesleyBrackett94@gmail.com>
2025-08-24 17:35:46 -0500

    simplified guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,22 +1,23 @@
-use std::io;
+use std::io::stdin;
 use rand::Rng;
+use std::cmp::Ordering::{Less, Greater, Equal};
 
 fn main() {
-    let mut rng = rand::rng();
-    let answer: u32 = rng.random_range(..100);
+    let answer: u32 = rand::rng().random_range(0..100);
     println!("Guess the number between 0 and 100");
     loop {
         println!("Your guess: ");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let parsed_guess = guess.trim().parse::<u32>().unwrap();
-        if parsed_guess < answer {
-            println!("Your guess is too low");
-        } else if parsed_guess > answer {
-            println!("Your guess is too high");
-        } else {
-            break;
-        }
+        stdin().read_line(&mut guess).expect("Failed to read line");
+        let parsed_guess = guess.trim().parse::<u32>().expect("Please type a number!");
+
+        match parsed_guess.cmp(&answer) {
+            Less => println!("Too low"),
+            Greater => println!("Too high"),
+            Equal => {
+                println!("You win!!!");
+                break;
+            }
+        };
     }
-    println!("YOU WON!");
 }
